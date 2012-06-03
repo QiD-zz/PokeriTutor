c791c49d64c94cf52f0fad19cd572a787808ddd9
Kuisma Kuusniemi <kuisma.kuusniemi@gmail.com>
2012-06-03 19:38:38 +0300

    raahauspaneeli listty

--- src/pokeri/MainMenu.java
+++ src/pokeri/MainMenu.java
@@ -4,6 +4,9 @@
  */
 package pokeri;
 
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
 import javax.swing.*;
 
 /**
@@ -13,30 +16,23 @@ import javax.swing.*;
 public class MainMenu extends JFrame{
     
     private JPanel vasenPaneeli;
-    private JButton raahauspeli;
-    private JPanel oikea;
-    private DefaultListModel listamalli;
-    private JList lista;
+    private JButton raahauspeliNappi;
+    private JButton testikorttibutton;
+    private RaahausPeliPaneeli raahausPeli;
     
     public MainMenu() {
         super("TaoTao menee metsään!");
         vasenPaneeli = new JPanel();
-        raahauspeli = new JButton("Raahauspeli");
-                
-        KasiTransferHandler handleri = new KasiTransferHandler();
-        oikea = new JPanel();
-        listamalli = new DefaultListModel();
-        lista = new JList(listamalli);
-        listamalli.addElement("Pari");
-        listamalli.addElement("Neloset");
-        listamalli.addElement("Suora");
-        listamalli.addElement("Väri");
-        vasenPaneeli.add(raahauspeli);
-        oikea.add(lista);
-        lista.setDragEnabled(true);
-        lista.setTransferHandler(handleri);
+        vasenPaneeli.setPreferredSize(new Dimension(200, 600));
+        vasenPaneeli.setBackground(Color.red);
+        raahauspeliNappi = new JButton("Raahauspeli");
+        testikorttibutton = new JButton("testikorttibutton");
+        raahausPeli = new RaahausPeliPaneeli();        
+        vasenPaneeli.add(raahauspeliNappi);
+        vasenPaneeli.add(testikorttibutton);
+        this.setLayout(new FlowLayout());
         this.getContentPane().add(vasenPaneeli);
-        this.getContentPane().add(oikea);
+        this.getContentPane().add(raahausPeli);
         this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         this.pack();
         
