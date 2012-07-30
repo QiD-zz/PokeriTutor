ba3b37e38ee94996d9f77f181bd6b7a8601b8dfb
hurvittelu <hurvittelu@192.168.50.204>
2012-07-30 15:40:17 +0300

    nyt kortit näkyy oikein ainakin mulla

--- src/opetus/OpiTuntemaanHanskat.java
+++ src/opetus/OpiTuntemaanHanskat.java
@@ -5,9 +5,11 @@
 package opetus;
 
 import java.awt.Dimension;
+import java.awt.FlowLayout;
 import java.awt.Graphics;
 import java.awt.Point;
 import java.io.EOFException;
+import javax.swing.JButton;
 import javax.swing.JPanel;
 import pokeri.Extern;
 import pokeri.Kortti;
@@ -20,18 +22,23 @@ public class OpiTuntemaanHanskat extends JPanel {
     
     public OpiTuntemaanHanskat() {
         
-        this.setPreferredSize(new Dimension(750, 200));
-                
+        this.setPreferredSize(new Dimension(600, 400));
+        this.setLayout(new FlowLayout(FlowLayout.LEFT, 0, 0));   
         Kortti[] kortit = new Kortti[5];
         
         for (int i = 0; i < kortit.length; i++) {
             int y = 10;
-            Point piste = new Point(0, y);
+            Point piste = new Point(10, y);
             kortit[i] = new Kortti(Extern.MAAT[0], 4+i, piste);
             this.add(kortit[i]);
-            kortit[i].paint(this.getGraphics());
+           // kortit[i].paint(this.getGraphics());
         }
-        
+        for (int i = 0; i < kortit.length; i++) {
+            JButton button = new JButton("Lukitse");
+            button.setPreferredSize(new Dimension(120, 50));
+            this.add(button);
+            
+        }
     }
     
 }
