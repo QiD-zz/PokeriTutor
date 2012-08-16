8d0ca911f884434a8574705c2806d132d93f9722
Ville Valkonen <ville.oskari.valkonen@uta.fi>
2012-08-16 22:06:11 +0300

    Pastellivärit etusivulle

--- src/pokeri/EtusivuPaneeli.java
+++ src/pokeri/EtusivuPaneeli.java
@@ -1,5 +1,6 @@
 package pokeri;
 
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.GridLayout;
@@ -19,15 +20,29 @@ public class EtusivuPaneeli extends JPanel
     {
         main = m;
         tkuuntelija = new Tapahtumakuuntelija(main);
-        
-        this.setPreferredSize(new Dimension(Extern.LEVEYS_IKKUNA, Extern.KORKEUS_IKKUNA));
-        this.setLayout(new FlowLayout(FlowLayout.CENTER));
-        
-        JButton opiKadet = new JButton("Opi tuntemaan pokerikädet");     
-        JButton kadetJarj = new JButton("Kädet järjestykseen -peli");      
-        JButton pelaaPokeri = new JButton("Käytännön pokeripeli");
-        JButton monivalintapeli = new JButton("Testaa taitosi");
-    
+        JButton opiKadet;
+        JButton kadetJarj;
+        JButton pelaaPokeri;
+        JButton monivalintapeli;
+
+        setPreferredSize(new Dimension(Extern.LEVEYS_IKKUNA, Extern.KORKEUS_IKKUNA));
+        setLayout(new FlowLayout(FlowLayout.CENTER));
+        setBackground(new Color(238, 232, 170));
+
+        opiKadet = new JButton("Opi tuntemaan pokerikädet");
+        opiKadet.setBackground(new Color(107, 142, 35));
+        opiKadet.setOpaque(true);
+        opiKadet.setFocusPainted(false);
+        kadetJarj = new JButton("Kädet järjestykseen -peli");
+        kadetJarj.setBackground(new Color(154, 205, 50));
+        kadetJarj.setOpaque(true);
+        pelaaPokeri = new JButton("Käytännön pokeripeli");
+        pelaaPokeri.setBackground(new Color(240, 230, 140));
+        pelaaPokeri.setOpaque(true);
+        monivalintapeli = new JButton("Testaa taitosi");
+        monivalintapeli.setBackground(new Color(189, 183, 107));
+        monivalintapeli.setOpaque(true);
+
         opiKadet.addActionListener(tkuuntelija);
         kadetJarj.addActionListener(tkuuntelija);
         pelaaPokeri.addActionListener(tkuuntelija);
@@ -36,12 +51,12 @@ public class EtusivuPaneeli extends JPanel
         kadetJarj.setActionCommand(Extern.RAAHAUSPELI);
         pelaaPokeri.setActionCommand(Extern.KORTTIPELI);
         monivalintapeli.setActionCommand(Extern.MONIVALINTAPELI);
-        
+
         opiKadet.setPreferredSize(new Dimension(Extern.LEVEYS_IKKUNA/4, Extern.KORKEUS_IKKUNA/10));
         kadetJarj.setPreferredSize(new Dimension(Extern.LEVEYS_IKKUNA/4, Extern.KORKEUS_IKKUNA/10));
         pelaaPokeri.setPreferredSize(new Dimension(Extern.LEVEYS_IKKUNA/4, Extern.KORKEUS_IKKUNA/10));
         monivalintapeli.setPreferredSize(new Dimension(Extern.LEVEYS_IKKUNA/4, Extern.KORKEUS_IKKUNA/10));
-        
+
         JPanel keskikaista = new JPanel(new FlowLayout(FlowLayout.CENTER));
         keskikaista.setPreferredSize(new Dimension(Extern.LEVEYS_IKKUNA/2, Extern.KORKEUS_IKKUNA/4));
         keskikaista.setLayout(new GridLayout(2, 2, 2, 2));
