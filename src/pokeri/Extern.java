f4928962faffdb70fc1b772b445a5095564f0eab
Ville Valkonen <ville.oskari.valkonen@uta.fi>
2012-08-16 22:02:38 +0300

    Pisteytystaulukko

--- src/pokeri/Extern.java
+++ src/pokeri/Extern.java
@@ -1,6 +1,9 @@
 package pokeri;
 
 import java.awt.Point;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class Extern
@@ -12,18 +15,40 @@ public class Extern
     public final static int LEVEYS_IKKUNA = 1000;
     public final static int KORKEUS_IKKUNA = 1000;
     public final static int VAIHTOJEN_LKM = 2;
-    public final static String[] MAAT = { "ruutu", "hertta", "pata", "risti" };
-
     public final static int SKENAARIOIDEN_LKM = 5;
+    public final static String[] MAAT = { "ruutu", "hertta", "pata", "risti" };
+    public final static Map<String, Integer> VOITTOPISTE =
+            Collections.unmodifiableMap(new HashMap<String, Integer>() {{
+        /*
+         * (Arvokkain ensin)
+         * Värisuora
+         * Neljä samaa
+         * Täyskäsi
+         * Väri
+         * Suora
+         * Kolme samaa
+         * Kaksi paria
+         * Pari
+         * Hai
+         */
+        put("varisuora", 100);
+        put("neloset", 80);
+        put("tayskasi", 50);
+        put("vari", 40);
+        put("suora", 40);
+        put("kolmoset", 20);
+        put("kaksiparia", 15);
+        put("pari", 10);
+    }});
+
     //komennot
     public final static String RAAHAUSPELI = "RaahausPeli";
     public final static String KORTTIPELI = "KorttiPeli";
     public final static String OPETUS = "Opetus";
     public final static String ETUSIVU = "Etusivulle";
-    public static final String MONIVALINTAPELI = "monivalintapeli";
+    public final static String MONIVALINTAPELI = "monivalintapeli";
+    public final static String UUSIMONIVALINTATEHTAVA = "uusi tehtävä";
+    public final static String MONIVALINTAVASTAUS = "vastasin, olinko oikeassa?";
 
-    public static final String UUSIMONIVALINTATEHTAVA = "uusi tehtävä";
-    public static final String MONIVALINTAVASTAUS = "vastasin, olinko oikeassa?";
-
-    public static final Point PERUSPISTE10X10Y = new Point(10, 10);
+    public final static Point PERUSPISTE10X10Y = new Point(10, 10);
 }
